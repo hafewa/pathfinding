75e1ca4483e84b1b17ce6c91c8ccf6e9018fe3d7
Romain Roy <rroy.romain@gmail.com>
2019-09-26 13:59:08 +0200

    Début projet

---
+++ Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour
+{
+	public int ligne = 10;
+	public int colonne = 10;
+	public Transform noeud;
+
+	public List<Transform> grille = new List<Transform>();
+
+	void Start()
+	{
+		this.genererGrille();
+		this.genererVoisins();
+	}
+
+	private void genererGrille()
+	{
+		for (int i = 0; i < ligne; i++)
+			for (int j = 0, k = 0; j < colonne; j++, k++)
+			{
+				Transform n = Instantiate(noeud, new Vector3(j + gameObject.transform.position.x, gameObject.transform.position.y, i + gameObject.transform.position.z), Quaternion.identity);
+				n.name = "noeud (" + k + ")";
+				grille.Add(n);
+			}
+	}
+
+	private void genererVoisins()
+	{
+		for (int i = 0, j = 1; i < grille.Count; i++, j++)
+		{
+			Noeud n = grille[i].GetComponent<Noeud>();
+			if (j % colonne == 0) // S'il n'y a pas de noeuds à droite
+			{
+				if (i + colonne < colonne * ligne) n.addVoisin(grille[i + colonne]); // Noeud du haut
+				if (i - colonne >= 0) n.addVoisin(grille[i - colonne]); // Noeud du bas
+				n.addVoisin(grille[i - 1]); // Noeud de gauche
+			}
+			else if (j % colonne == 1) // S'il n'y a pas de noeuds à gauche
+			{
+				if (i + colonne < colonne * ligne) n.addVoisin(grille[i + colonne]); // Noeud du haut
+				if (i - colonne >= 0) n.addVoisin(grille[i - colonne]); // Noeud du bas
+				n.addVoisin(grille[i + 1]); // Noeud à droite
+			}
+			else
+			{
+				if (i + colonne < colonne * ligne) n.addVoisin(grille[i + colonne]); // Noeud du haut
+				if (i - colonne >= 0) n.addVoisin(grille[i - colonne]); // Noeud du bas
+				n.addVoisin(grille[i - 1]); // Noeud de gauche
+				n.addVoisin(grille[i + 1]); // Noeud de droite
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Noeud.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noeud : MonoBehaviour
+{
+	[SerializeField] private float poids = int.MaxValue;
+	[SerializeField] private Transform parent = null;
+	[SerializeField] private List<Transform> voisins;
+	[SerializeField] private bool libre = true;
+
+	void Start()
+	{
+		this.reset();
+	}
+
+	public void reset()
+	{
+		parent = null;
+		poids = int.MaxValue;
+	}
+
+	public void setParent(Transform n)
+	{
+		this.parent = n;
+	}
+
+	public void setPoids(float p)
+	{
+		this.poids = p;
+	}
+
+	public void setLibre(bool b)
+	{
+		this.libre = b;
+	}
+
+	public void addVoisin(Transform n)
+	{
+		this.voisins.Add(n);
+	}
+
+	public List<Transform> getVoisins()
+	{
+		return this.voisins;
+	}
+
+	public float getPoids()
+	{
+		return this.poids;
+	}
+
+	public Transform getParent()
+	{
+		return this.parent;
+	}
+
+	public bool isLibre()
+	{
+		return this.libre;
+	}
+}
