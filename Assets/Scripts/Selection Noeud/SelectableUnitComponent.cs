570e6cf284b1758b22d414eb8df64dfebdef027a
Romain Roy <rroy.romain@gmail.com>
2019-09-26 14:36:45 +0200

    Avancement

---
+++ Assets/Scripts/Selection Noeud/SelectableUnitComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectableUnitComponent : MonoBehaviour
+{
+	[SerializeField] private bool selection = false;
+
+	public void setSelection(bool s)
+	{
+		this.selection = s;
+	}
+
+	public bool isSelected()
+	{
+		return selection;
+	}
+}
---
+++ Assets/Scripts/Selection Noeud/UnitSelectionComponent.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+
+public class UnitSelectionComponent : MonoBehaviour
+{
+	private bool isSelecting = false;
+	private Vector3 mousePosition1;
+	private List<Transform> selectedObjects = new List<Transform>();
+
+	private void unitSelectionSystem()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			isSelecting = true;
+			mousePosition1 = Input.mousePosition;
+			foreach (var selectableObject in FindObjectsOfType<SelectableUnitComponent>())
+				if (selectableObject.isSelected())
+				{
+					Renderer rend = selectableObject.GetComponent<Renderer>();
+					rend.material.color = Color.white;
+					selectableObject.setSelection(false);
+				}
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			foreach (var selectableObject in FindObjectsOfType<SelectableUnitComponent>())
+				if (this.isInBound(selectableObject.gameObject))
+					selectedObjects.Add(selectableObject.transform);
+			isSelecting = false;
+		}
+		if (isSelecting)
+		{
+			foreach (var selectableObject in FindObjectsOfType<SelectableUnitComponent>())
+			{
+				if (this.isInBound(selectableObject.gameObject))
+				{
+					if (!selectableObject.isSelected())
+					{
+						selectableObject.setSelection(true);
+						Renderer rend = selectableObject.GetComponent<Renderer>();
+						rend.material.color = Color.green;
+					}
+				}
+				else
+				{
+					if (selectableObject.isSelected())
+					{
+						Renderer rend = selectableObject.GetComponent<Renderer>();
+						rend.material.color = Color.white;
+						selectableObject.setSelection(false);
+					}
+				}
+			}
+		}
+	}
+
+	private bool isInBound(GameObject gameObject)
+	{
+		if (!isSelecting) return false;
+		var camera = Camera.main;
+		var viewportBounds = Utils.GetViewportBounds(camera, mousePosition1, Input.mousePosition);
+		return viewportBounds.Contains(camera.WorldToViewportPoint(gameObject.transform.position));
+	}
+
+	public List<Transform> getSelectedObjects()
+	{
+		List<Transform> result = selectedObjects;
+		return result;
+	}
+
+	public void clearSelections()
+	{
+		this.selectedObjects.Clear();
+	}
+
+	void OnGUI()
+	{
+		if (isSelecting)
+		{
+			var rect = Utils.GetScreenRect(mousePosition1, Input.mousePosition);
+			Utils.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+			Utils.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
+		}
+	}
+
+	void Update()
+	{
+		this.unitSelectionSystem();
+	}
+}
